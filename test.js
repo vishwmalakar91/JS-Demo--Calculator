7b03470efe74f41e20cc45070f2721c92198faae
Vishwanath Malakar <VMALAKAR@amdocs.com>
2018-09-06 16:10:33 +0530

    adding new files

---
+++ test.js
@@ -0,0 +1,25 @@
+function add_numbers(a, b) {
+c=a+b;
+alert(a+"+"+b+"=>" + c);
+}
+
+function min() {
+		a = document.getElementById("input1").value;
+		b = document.getElementById("input2").value;
+		c=(a)-(b);
+	alert(a+"-"+b+"=>" + c);
+}
+
+function mul() {
+	a = document.getElementById("input1").value;
+	b = document.getElementById("input2").value;
+	d=(a)*(b);
+alert(a+"x"+b+"=>" + d);
+}
+
+function add() {
+	a = document.getElementById("input1").value;
+	b = document.getElementById("input2").value;
+	d=(a+b);
+alert(a+"+"+b+"=>" + d);
+}
